0b07e4fe828f273103687316aa075ce24dc7e0e3
arsenyan <artyom@arsenyan.ru>
2024-07-29 13:58:31 +0200

    SEO upd v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,13 +16,9 @@ export const metadata: Metadata = {
     siteName: "Annemarillis Invest",
     description: "At Annemarillis Invest, we leverage over 25 years of experience in Management Consulting, Investment Strategy, M&A, Corporate Finance, and Social Entrepreneurship to provide family office services.",
     url: "https://annemarillis.com",
-    images: [{
-      url: "https://annemarillis.com/og-image.png",
-    }],
   },
   twitter: {
-    card: "summary_large_image",
-    "images": "https://annemarillis.com/og-image.png"
+    card: "summary_large_image"
   }
 };
 
