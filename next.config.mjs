e68e5f9768d41a8e3f008e6dce45d0dd7ee641ae
Your Name <39626348+arsenyan@users.noreply.github.com>
2025-08-18 12:27:08 +0200

    Update configuration for GitHub Pages deployment and modify .gitignore

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/annemarilis-invest-web', // if the site is not on user.github.io, but on user.github.io/repo
+  images: {
+    unoptimized: true, // disable optimization because GitHub Pages doesn't support it
+  },
+};
 
 export default nextConfig;
